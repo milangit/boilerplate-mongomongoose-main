39c9f81168c9bc5966644785fa8f09a9eb12810b
milan1248 <miba1248@gmail.com>
2023-11-30 22:24:10 +0100

    fix

--- myApp.js
+++ myApp.js
@@ -76,16 +76,16 @@ Person = mongoose.model('PersonModel', personSchema);
 // /* You completed these challenges, let's go celebrate !
 //  */
 
-// //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
+//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
 
 exports.PersonModel = Person;
-// exports.createAndSavePerson = createAndSavePerson;
-// exports.findPeopleByName = findPeopleByName;
-// exports.findOneByFood = findOneByFood;
-// exports.findPersonById = findPersonById;
-// exports.findEditThenSave = findEditThenSave;
-// exports.findAndUpdate = findAndUpdate;
-// exports.createManyPeople = createManyPeople;
-// exports.removeById = removeById;
-// exports.removeManyPeople = removeManyPeople;
-// exports.queryChain = queryChain;
+exports.createAndSavePerson = createAndSavePerson;
+exports.findPeopleByName = findPeopleByName;
+exports.findOneByFood = findOneByFood;
+exports.findPersonById = findPersonById;
+exports.findEditThenSave = findEditThenSave;
+exports.findAndUpdate = findAndUpdate;
+exports.createManyPeople = createManyPeople;
+exports.removeById = removeById;
+exports.removeManyPeople = removeManyPeople;
+exports.queryChain = queryChain;
