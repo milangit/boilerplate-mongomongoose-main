233561c5bf9e1c7ac795d87597bed8b788c27d12
milan1248 <miba1248@gmail.com>
2023-11-30 21:47:00 +0100

    connect

--- myApp.js
+++ myApp.js
@@ -1,106 +1,110 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
 let mongoose = require('mongoose');
 
 const uri = process.env.MONGO_URI
 
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-    useUnifiedTopology: true 
-  }
-});
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//     useUnifiedTopology: true 
+//   }
+// });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-
-
-run().catch(console.dir);
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
 
 
+// run().catch(console.dir);
 
 
 
 
-require('dotenv').config();
 
 
-let Person;
+// require('dotenv').config();
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
-};
 
-const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
-};
+// let Person;
 
-const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
-};
+// const createAndSavePerson = (done) => {
+//   done(null /*, data*/);
+// };
 
-const findOneByFood = (food, done) => {
-  done(null /*, data*/);
-};
+// const createManyPeople = (arrayOfPeople, done) => {
+//   done(null /*, data*/);
+// };
 
-const findPersonById = (personId, done) => {
-  done(null /*, data*/);
-};
+// const findPeopleByName = (personName, done) => {
+//   done(null /*, data*/);
+// };
 
-const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
+// const findOneByFood = (food, done) => {
+//   done(null /*, data*/);
+// };
 
-  done(null /*, data*/);
-};
+// const findPersonById = (personId, done) => {
+//   done(null /*, data*/);
+// };
 
-const findAndUpdate = (personName, done) => {
-  const ageToSet = 20;
+// const findEditThenSave = (personId, done) => {
+//   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
-};
+//   done(null /*, data*/);
+// };
 
-const removeById = (personId, done) => {
-  done(null /*, data*/);
-};
+// const findAndUpdate = (personName, done) => {
+//   const ageToSet = 20;
 
-const removeManyPeople = (done) => {
-  const nameToRemove = "Mary";
+//   done(null /*, data*/);
+// };
 
-  done(null /*, data*/);
-};
+// const removeById = (personId, done) => {
+//   done(null /*, data*/);
+// };
 
-const queryChain = (done) => {
-  const foodToSearch = "burrito";
+// const removeManyPeople = (done) => {
+//   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
-};
+//   done(null /*, data*/);
+// };
 
-/** **Well Done !!**
-/* You completed these challenges, let's go celebrate !
- */
+// const queryChain = (done) => {
+//   const foodToSearch = "burrito";
 
-//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
+//   done(null /*, data*/);
+// };
 
-exports.PersonModel = Person;
-exports.createAndSavePerson = createAndSavePerson;
-exports.findPeopleByName = findPeopleByName;
-exports.findOneByFood = findOneByFood;
-exports.findPersonById = findPersonById;
-exports.findEditThenSave = findEditThenSave;
-exports.findAndUpdate = findAndUpdate;
-exports.createManyPeople = createManyPeople;
-exports.removeById = removeById;
-exports.removeManyPeople = removeManyPeople;
-exports.queryChain = queryChain;
+// /** **Well Done !!**
+// /* You completed these challenges, let's go celebrate !
+//  */
+
+// //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
+
+// exports.PersonModel = Person;
+// exports.createAndSavePerson = createAndSavePerson;
+// exports.findPeopleByName = findPeopleByName;
+// exports.findOneByFood = findOneByFood;
+// exports.findPersonById = findPersonById;
+// exports.findEditThenSave = findEditThenSave;
+// exports.findAndUpdate = findAndUpdate;
+// exports.createManyPeople = createManyPeople;
+// exports.removeById = removeById;
+// exports.removeManyPeople = removeManyPeople;
+// exports.queryChain = queryChain;
